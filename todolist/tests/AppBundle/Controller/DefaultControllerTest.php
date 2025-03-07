a7829d352c1c5dc612dfdd88246900a289c4872e
DESKTOP-9MTB132\Aurel <demblans.dev@gmail.com>
2025-03-07 01:12:19 +0100

    add tests

---
+++ todolist/tests/AppBundle/Controller/DefaultControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('#container h1', 'Welcome to Symfony');
+    }
+}
